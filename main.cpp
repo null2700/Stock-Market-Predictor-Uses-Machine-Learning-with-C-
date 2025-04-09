105e0b31eafd9895c190c682b0a4180179f322b4
null2700 <guravsoham182@gmail.com>
2025-04-09 20:05:21 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,22 +7,22 @@ using namespace arma;
 using namespace std;
 
 int main() {
-    // Load the dataset (CSV file with historical stock data)
+    
     mat data;
     data::Load("stock_data.csv", data, true);
 
-    // Split into features (X) and target (Y)
+    
     mat X = data.submat(0, 0, data.n_rows - 2, data.n_cols - 1);
     rowvec Y = data.row(data.n_rows - 1);
 
-    // Train a Linear Regression model
+    
     LinearRegression lr(X, Y);
 
-    // Predict next values (for testing, we use the same features)
+   
     rowvec predictions;
     lr.Predict(X, predictions);
 
-    // Save predictions to a file
+    
     data::Save("predictions.csv", predictions);
     cout << "Stock Price Predictions Saved!" << endl;
 
